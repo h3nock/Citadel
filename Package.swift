70aa5b412e9d56bdc1e04671fc675b0e5f529b2d
henok3878 <81500872+henok3878@users.noreply.github.com>
2026-08-02 17:57:02 -0400

    update nio ssh local close fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // .package(path: "/Users/joannisorlandos/git/joannis/swift-nio-ssh"),
-        .package(url: "https://github.com/h3nock/swift-nio-ssh.git", revision: "7588777b8f6439efa1a33117f86cb2729abd864c"),
+        .package(url: "https://github.com/h3nock/swift-nio-ssh.git", revision: "450dad17bf746843f5ecd2fca32c4ee61a354d13"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.0"),
